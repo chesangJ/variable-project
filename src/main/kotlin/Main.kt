c98a76b8c79811ebd381ad534f7c7b0549f015fa
Mitingi Joy <mitingijoychesang@gmail.com>
2023-02-02 07:17:45 +0300

    created student project

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>) {
+    var fullname = "joy c"
+    var age = 45
+    var phoneNumber = "0742765897"
+    var weightInKgs = 62
+    var isKenyan: Boolean = true
+    var notKenyan: Boolean = false
+
+    println(isKenyan)
+}
+
+
+
+
+
+
+
+
+
+
+
